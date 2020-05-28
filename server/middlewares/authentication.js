a29f1ae28f8aaf8674c80a69b40723b8c116bbbb
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2020-05-28 14:59:16 -0400

    Updated auth
    edit to make google auth work in heroku

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -22,7 +22,7 @@ passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'passwor
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/api/user/oauth/google/callback"
+    callbackURL: "/api/user/oauth/google/callback"
   }, (accessToken, refreshToken, profile, cb) => {
     const displayName = profile.displayName;
     const email = profile.emails[0].value;
